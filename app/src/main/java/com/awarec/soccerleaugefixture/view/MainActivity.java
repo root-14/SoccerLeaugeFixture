c6927aa07c719809ed4c49dd9ce3abd9e13806dc
root14 <ilkaygavaz2@gmail.com>
2021-06-12 19:01:10 +0300

    ViewPager working with local data 12/06/2021

--- app/src/main/java/com/awarec/soccerleaugefixture/view/MainActivity.java
+++ app/src/main/java/com/awarec/soccerleaugefixture/view/MainActivity.java
@@ -1,10 +1,12 @@
 package com.awarec.soccerleaugefixture.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
 import com.awarec.soccerleaugefixture.R;
+import com.awarec.soccerleaugefixture.viewmodel.ViewPagerAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,15 +14,38 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    List<String> team_names = new ArrayList<>();
+    String[] team_names;
+
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        viewPager = findViewById(R.id.viewpager);
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(this, fillList(6));
+        viewPager.setAdapter(viewPagerAdapter);
     }
 
+
+    public static String[] fillList(int count) {
+
+        String array[] = new String[count];
+
+        for (int i = 0; i < count; i++) {
+
+            array[i] = "team" + (i + 1);
+
+            // System.out.println(array[i]);
+
+        }
+
+        System.out.println("length: " + array.length);
+        return array;
+    }
 
 
 }
--- app/src/main/java/com/awarec/soccerleaugefixture/viewmodel/ViewPagerAdapter.java
+++ app/src/main/java/com/awarec/soccerleaugefixture/viewmodel/ViewPagerAdapter.java
@@ -1,12 +1,67 @@
 package com.awarec.soccerleaugefixture.viewmodel;
 
-import androidx.viewpager.widget.PagerAdapter;
-import androidx.viewpager2.widget.ViewPager2;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-import java.util.List;
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import com.awarec.soccerleaugefixture.R;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 public class ViewPagerAdapter extends PagerAdapter {
 
-    List<String> team_names;
+    private Context context;
+    private String[] team_names;
 
+
+    public ViewPagerAdapter(Context context,  String[] team_names) {
+        this.context = context;
+        this.team_names = team_names;
+    }
+
+
+    @Override
+    public int getCount() {
+        return team_names.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+
+        return view.equals(object);
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+
+        View view = LayoutInflater.from(context).inflate(R.layout.item_view_pager, container, false);
+
+        TextView tv_week, tv_matches;
+
+        tv_week = view.findViewById(R.id.tv_week);
+        tv_matches = view.findViewById(R.id.tv_matches);
+
+        tv_matches.setText(team_names[position]);
+        tv_week.setText("Week: " + position);
+
+        //container.addView(view, position);
+        container.addView(view);
+
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, Object object) {
+
+        container.removeView((View) object);
+        //super.destroyItem(container, position, object);
+    }
 }
