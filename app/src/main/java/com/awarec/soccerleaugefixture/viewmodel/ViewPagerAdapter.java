c7d38437ceafb70670da320972d3c868af60ed62
root14 <ilkaygavaz2@gmail.com>
2021-06-12 18:22:31 +0300

    UI Design 12/06/2021

--- app/src/main/java/com/awarec/soccerleaugefixture/viewmodel/ViewPagerAdapter.java
+++ app/src/main/java/com/awarec/soccerleaugefixture/viewmodel/ViewPagerAdapter.java
@@ -1,10 +1,11 @@
 package com.awarec.soccerleaugefixture.viewmodel;
 
+import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
 import java.util.List;
 
-public class ViewPagerAdapter {
+public class ViewPagerAdapter extends PagerAdapter {
 
     List<String> team_names;
 
